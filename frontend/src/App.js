6872930800f1f350437198a0b8654a9dc5fb7956
Darshan Bankar <darshanbankar1915@gmail.com>
2024-09-19 01:07:42 +0530

    19/9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,112 +1,33 @@
-// //  import './App.css';
-// //  import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-// // import Signup from './components/Signup';
-// // import Login from './components/Login';
-// // import HomePage from './components/HomePage';
+import './App.css';
+import { createBrowserRouter,RouterProvider} from "react-router-dom"
+import Login from "./components/Login.jsx"
+import HomePage from './components/HomePage.jsx';
+import Signup from './components/Signup.jsx';
 
-// //  const router = createBrowserRouter([
-// //   {
-// //     path:"/",
-// //     element:<HomePage/>
-// //   },
-// //   {
-// //     path:"/register",
-// //     element:<Signup/>
-// //   },
-// //   {
-// //     path:"/login",
-// //     element:<Login/>
-// //   }
-// //  ])
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomePage/>
+  },
+  {
+    path:"/signup",
+    element:<Signup/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
 
-// // function App() {
-// //   return (
-// //     <div className="flex items-center h-screen p-4">
-// //         <RouterProvider router={router}/>
-// //     </div>
-// //   );
-// // }
+])
 
-// // export default App;
+function App() {
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+            <RouterProvider router={router}></RouterProvider>
 
+    </div>
+  );
+}
 
+export default App;
  
-
-
-
-
-
-// import Signup from './components/Signup';
-// import './App.css';
-// import {createBrowserRouter, RouterProvider} from "react-router-dom";
-// import HomePage from './components/HomePage';
-// import Login from './components/Login';
-// import { useEffect, useState } from 'react';
-// import {useSelector,useDispatch} from "react-redux";
-// import io from "socket.io-client";
-// import { setSocket } from './redux/socketSlice';
-// import { setOnlineUsers } from './redux/userSlice';
-// import { BASE_URL } from '.';
-
-// const router = createBrowserRouter([
-//   {
-//     path:"/",
-//     element:<HomePage/>
-//   },
-//   {
-//     path:"/signup",
-//     element:<Signup/>
-//   },
-//   {
-//     path:"/login",
-//     element:<Login/>
-//   },
-
-// ])
-
-// function App() { 
-//   const {authUser} = useSelector(store=>store.user);
-//   const {socket} = useSelector(store=>store.socket);
-//   const dispatch = useDispatch();
-
-//   useEffect(()=>{
-//     if(authUser){
-//       const socketio = io(`${BASE_URL}`, {
-//           query:{
-//             userId:authUser._id
-//           }
-//       });
-//       dispatch(setSocket(socketio));
-
-//       socketio?.on('getOnlineUsers', (onlineUsers)=>{
-//         dispatch(setOnlineUsers(onlineUsers))
-//       });
-//       return () => socketio.close();
-//     }else{
-//       if(socket){
-//         socket.close();
-//         dispatch(setSocket(null));
-//       }
-//     }
-
-//   },[authUser]);
-
-//   return (
-//     <div className="flex items-center justify-center h-screen p-4">
-//       <RouterProvider router={router}/>
-//     </div>
-
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
